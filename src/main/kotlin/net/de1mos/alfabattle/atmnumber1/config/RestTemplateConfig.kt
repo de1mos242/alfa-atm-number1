c771659bf5b18f734e2d8ead2f90411db4cf31a3
Denis Yakovlev <de1m0s242@gmail.com>
2020-06-27 18:30:03 +0700

    second attempt

--- src/main/kotlin/net/de1mos/alfabattle/atmnumber1/config/RestTemplateConfig.kt
+++ src/main/kotlin/net/de1mos/alfabattle/atmnumber1/config/RestTemplateConfig.kt
@@ -1,5 +1,7 @@
 package net.de1mos.alfabattle.atmnumber1.config
 
+import org.apache.commons.io.FileUtils
+import org.apache.commons.io.IOUtils
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.ssl.SSLContextBuilder
@@ -9,6 +11,8 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.Resource
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.web.client.RestTemplate
+import java.io.File
+import java.io.InputStream
 import javax.net.ssl.SSLContext
 
 
@@ -23,8 +27,13 @@ class RestTemplateConfig {
 
     @Bean
     fun restTemplate(): RestTemplate? {
+        val jks = File.createTempFile("server_keystore", ".jks")
+        keyStore.inputStream.use {
+            FileUtils.copyInputStreamToFile(it, jks)
+        }
+
         val sslContext: SSLContext = SSLContextBuilder()
-                .loadKeyMaterial(keyStore.file, keyStorePassword.toCharArray(), keyStorePassword.toCharArray())
+                .loadKeyMaterial(jks, keyStorePassword.toCharArray(), keyStorePassword.toCharArray())
                 .build()
         val socketFactory = SSLConnectionSocketFactory(sslContext)
         val httpClient = HttpClients.custom()
